61bfe3d670be0df37e0b5fba9ace62f62d0a1c79
sojood <sojood.alsayyed@gmail.com>
2019-07-15 13:51:17 +0300

    Clear

--- config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: 'mongodb://localhost:27017/trello_db',
-    db_dev: 'mongodb://localhost:27017/trello_db',
-};
-
-/*module.exports = {
-    db: 'mongodb+srv://FutnaShoqo:futna@cluster0-t1hpa.mongodb.net/test?retryWrites=true&w=majority',
-    db_dev: 'mongodb+srv://FutnaShoqo:futna@cluster0-t1hpa.mongodb.net/test?retryWrites=true&w=majority',
-};*/
