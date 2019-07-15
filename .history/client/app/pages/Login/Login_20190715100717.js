a0d76c4f5a2c0492f83b1cf9256f13d67a4f07e0
sojood <sojood.alsayyed@gmail.com>
2019-07-15 10:08:12 +0300

    add build file

--- .history/client/app/pages/Login/Login_20190715100717.js
+++ .history/client/app/pages/Login/Login_20190715100717.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { NavLink, Redirect } from "react-router-dom";
-//import "../Login/login.scss";
-import "../../pages/Login/login.scss";
+import "../Login/login.scss";
+
 import google_icon from "../../assets/images/google_Icon.png";
 
 export default class Login extends Component {
